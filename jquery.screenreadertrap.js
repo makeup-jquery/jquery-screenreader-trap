fb3d41e5fb24b96d9d42844a14e220080939c19c
Ian McBurnie <imcburnie@ebay.com>
2016-02-17 14:09:25 -0800

    Added JSCS, JSDOC and PRE-COMMIT

--- jquery.screenreadertrap.js
+++ jquery.screenreadertrap.js
@@ -1,43 +1,96 @@
 /**
-* @name jquery-screenreader-trap
-* @function $.trapScreenreader
-* @version 0.2.0
-* @author Ian McBurnie <ianmcburnie@hotmail.com>
-* @desc Restricts virtual cursor to the given element and it's children. This is
-* achieved by adding aria-hidden=true to all siblings and ancestor siblings of
-* the trapped element. Any elements that have aria-hidden=true state before
-* the trap is activated must have their state preserved. Note this plugin does
-* not trap regular keyboard focus, use jquery-keyboard-trap instead.
-* @fires screenreaderTrap
-* @fires screenreaderUntrap
-*/
-(function ($, window, document, undefined) {
+ * @file jQuery singleton traps virtual cursor to the given element and it's children
+ * @author Ian McBurnie <ianmcburnie@hotmail.com>
+ */
 
-    var $trappedEl,
-        $modifiedElements;
+(function($, window, document, undefined) {
+
+    var pluginName = 'jquery-screenreader-trap';
+
+    /**
+    * jQuery definition to anchor JsDoc comments.
+    *
+    * @see http://jquery.com/
+    * @name $
+    * @class jQuery Library
+    */
+
+    /**
+    * jQuery 'fn' definition to anchor JsDoc comments.
+    *
+    *
+    * @see http://jquery.com/
+    * @name fn
+    * @class jQuery Plugin Scope
+    * @memberof jQuery
+    */
+
+    /**
+    * jQuery singleton traps keyboard focus cycle within given element's interactive children
+    *
+    * @class trapScreenreader
+    * @version 0.2.1
+    * @fires screenreaderTrap - when trap is activated
+    * @fires screenreaderUntrap - when trap is deactivated
+    * @return {jQuery} chainable jQuery class
+    * @memberof jQuery.fn
+    */
+
+    /**
+    * keyboardTrap event
+    *
+    * @event screenreaderTrap
+    * @type {object}
+    * @property {object} event - event object
+    * @memberof jQuery.fn.trapScreenreader
+    */
+
+    /**
+    * keyboardUntrap event
+    *
+    * @event screenreaderUntrap
+    * @type {object}
+    * @property {object} event - event object
+    * @memberof jQuery.fn.trapScreenreader
+    */
+
+    // the element that will be trapped
+    var $trappedEl;
+
+    // collection of elements that will be modified
+    var $modifiedElements;
 
     $.trapScreenreader = function trapScreenReader(el) {
+        // ensure current trap is deactivated
         $.untrapScreenreader();
 
+        // store reference to trapped el
         $trappedEl = $(el);
 
+        // find all the elements we need to hide, and all the elements we must keep unhidden
         var $siblings = $trappedEl.siblings(':not(script, [aria-hidden=true])');
         var $parents = $trappedEl.parents(':not(html, body)');
         var $parentsSiblings = $trappedEl.parents(':not(html, body)').siblings(':not(script, [aria-hidden=true])');
 
+        // update DOM
         $trappedEl.attr('aria-hidden', 'false');
         $siblings.attr('aria-hidden', 'true');
         $parents.attr('aria-hidden', 'false');
         $parentsSiblings.attr('aria-hidden', 'true');
 
+        // store reference to collection of elements that have been modified
         $modifiedElements = $trappedEl.add($siblings).add($parents).add($parentsSiblings);
 
+        // notify observers
         $trappedEl.trigger('screenreaderTrap');
     };
 
     $.untrapScreenreader = function untrapScreenReader() {
         if ($trappedEl) {
+            // restore modified elements to their original state
             $modifiedElements.removeAttr('aria-hidden');
+
+            // notify observers
             $trappedEl.trigger('screenreaderUntrap');
         }
     };
