f4ea2cbedba783c0168f0241bc312dfa1ad5f74c
ʌoʞɥsʎɹ qǝlƃ <gleb@ryshkov.com>
2017-01-25 08:17:52 -0800

    Removed cached trapped element after untrapping (#1)

--- test-setup.js
+++ test-setup.js
@@ -7,6 +7,16 @@ function setupSuite(html, options) {
     $body = $('body');
     $trap = $('.trap');
     $hasExistingState = $('.has-existing-state');
+    trapNotified = [];
+    untrapNotified = [];
+
+    $trap.on('screenreaderTrap', function () {
+        trapNotified.push(true);
+    });
+
+    $trap.on('screenreaderUntrap', function () {
+        untrapNotified.push(true);
+    });
 
     // execute plugin on widget
     $.trapScreenreader($trap, options);
